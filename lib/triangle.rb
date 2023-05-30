e32713f1d884f026e47c6dae24af8cbdae2ec233
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-05-30 11:05:20 +0300

    Done with Deliverables

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+    class TriangleError < StandardError
+  end
+
+  def initialize(length, width, height)
+    @length = length
+    @width = width
+    @height = height
+  end
+
+  def kind
+    validate_triangle
+
+    if @length == @width && @width == @height
+      :equilateral
+    elsif @length == @width || @length == @height || @width == @height
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    sides = [@length, @width, @height].sort
+    if sides.min <= 0 || sides[0] + sides[1] <= sides[2]
+      raise TriangleError, 'Invalid triangle'
+    end
+  end
 end
+
+# triangle = Triangle.new(7, 7, 5)
+# triangle_kind = triangle.kind
+
+# puts triangle_kind
